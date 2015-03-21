54ca9497590e5b11ce9a3286b23477305fb2f329
Ben Gelsey <ben@bengelsey.com>
2015-03-21 16:48:23 -0700

    Auto build before deploying

--- config.rb
+++ config.rb
@@ -85,4 +85,5 @@ activate :deploy do |deploy|
   deploy.branch   = 'master' # default: gh-pages
   # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
   # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
+  deploy.build_before = true
 end
